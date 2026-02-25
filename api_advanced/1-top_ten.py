8c67bf37944e9c0d4f097f5d50b63000deb642ed
AdukNyang <a.nyang@alustudent.com>
2026-02-25 19:37:12 +0300

    Add error handling and timeout to task 1

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -5,13 +5,21 @@ import requests
 
 def top_ten(subreddit):
     """Prints titles of first 10 hot posts for a given subreddit"""
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    if subreddit is None or not isinstance(subreddit, str):
+        print("None")
+        return
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {"User-Agent": "linux:1-top_ten:v1.0.0 (by /u/bdov_)"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        posts = data.get("data", {}).get("children", [])
-        for post in posts:
-            print(post.get("data", {}).get("title"))
-    else:
+    params = {"limit": 10}
+    try:
+        response = requests.get(url, headers=headers, params=params,
+                                allow_redirects=False, timeout=5)
+        if response.status_code == 200:
+            data = response.json()
+            posts = data.get("data", {}).get("children", [])
+            for post in posts:
+                print(post.get("data", {}).get("title"))
+        else:
+            print("None")
+    except Exception:
         print("None")
