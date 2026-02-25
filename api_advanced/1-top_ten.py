4c3cf76e6e18024f05cb29e6cc4a9fd7cb78d73f
AdukNyang <a.nyang@alustudent.com>
2026-02-25 19:39:32 +0300

    Simplify User-Agent for task 1

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -9,11 +9,11 @@ def top_ten(subreddit):
         print("None")
         return
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {"User-Agent": "linux:1-top_ten:v1.0.0 (by /u/bdov_)"}
+    headers = {"User-Agent": "0xDE4DBEE3"}
     params = {"limit": 10}
     try:
         response = requests.get(url, headers=headers, params=params,
-                                allow_redirects=False, timeout=5)
+                                allow_redirects=False)
         if response.status_code == 200:
             data = response.json()
             posts = data.get("data", {}).get("children", [])
